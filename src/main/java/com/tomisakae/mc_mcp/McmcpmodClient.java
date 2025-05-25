9d0bf2f4e89e13676a862f4fc6f6407913710b43
TomiSakae <tomisakaeap3@gmail.com>
2025-05-25 15:25:59 +0700

    Initial commit

---
+++ src/main/java/com/tomisakae/mc_mcp/McmcpmodClient.java
@@ -0,0 +1,10 @@
+package com.tomisakae.mc_mcp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McmcpmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Code dành riêng cho client (ví dụ: đăng ký renderer) sẽ ở đây
+    }
+}
